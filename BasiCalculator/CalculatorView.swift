baccc212a3df6fda6a4f16ef18b23b3813f5ecb1
yisslu <lugohalo999@gmail.com>
2024-10-23 20:08:09 -0600

    basic calculator

--- BasiCalculator/CalculatorView.swift
+++ BasiCalculator/CalculatorView.swift
@@ -22,8 +22,33 @@ class CalculatorView: UIView {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
-        stackView.backgroundColor = .cyan
+        stackView.spacing = 16
+        stackView.distribution = .fillEqually
         return stackView
+    }()
+    
+    lazy var buttonsNumbersAndSymbols: [UIButton] = {
+        var buttons: [UIButton] = []
+        buttons.append(createBasicButton(for: "AC", with: .systemGray3))
+        buttons.append(createBasicButton(for: "+/-", with: .systemGray3))
+        buttons.append(createBasicButton(for: "%", with: .systemGray3))
+        buttons.append(createBasicButton(for: "/", with: .systemYellow))
+        buttons.append(createBasicButton(for: "7", with: .systemGray))
+        buttons.append(createBasicButton(for: "8", with: .systemGray))
+        buttons.append(createBasicButton(for: "9", with: .systemGray))
+        buttons.append(createBasicButton(for: "x", with: .systemYellow))
+        buttons.append(createBasicButton(for: "4", with: .systemGray))
+        buttons.append(createBasicButton(for: "5", with: .systemGray))
+        buttons.append(createBasicButton(for: "6", with: .systemGray))
+        buttons.append(createBasicButton(for: "-", with: .systemYellow))
+        buttons.append(createBasicButton(for: "1", with: .systemGray))
+        buttons.append(createBasicButton(for: "2", with: .systemGray))
+        buttons.append(createBasicButton(for: "3", with: .systemGray))
+        buttons.append(createBasicButton(for: "+", with: .systemYellow))
+        buttons.append(createBasicButton(for: "0", with: .systemGray, tamic: false))
+        buttons.append(createBasicButton(for: ".", with: .systemGray, tamic: true))
+        buttons.append(createBasicButton(for: "=", with: .systemYellow, tamic: true))
+        return buttons
     }()
     
     override init(frame: CGRect){
@@ -31,6 +56,7 @@ class CalculatorView: UIView {
         addViews()
         addLabelResult()
         addMainStackView()
+        createMainStack()
     }
 
     required init?(coder: NSCoder) {
@@ -40,23 +66,75 @@ class CalculatorView: UIView {
     private func addLabelResult(){
         NSLayoutConstraint.activate([
             labelResult.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 0),
-            labelResult.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,constant: 0),
-            labelResult.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
-            labelResult.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.4)
+            labelResult.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,constant: 8),
+            labelResult.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            labelResult.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.30)
         ])
     }
     
     private func addMainStackView(){
         NSLayoutConstraint.activate([
             mainStackView.topAnchor.constraint(equalTo: labelResult.bottomAnchor),
-            mainStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-            mainStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-            mainStackView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.6)
+            mainStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            mainStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            mainStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         ])
     }
     
     private func addViews(){
         addSubview(labelResult)
         addSubview(mainStackView)
+    }
+    
+    private func createMainStack(){
+        let firstCell = createStackNumbers(first: buttonsNumbersAndSymbols[0], second: buttonsNumbersAndSymbols[1], third: buttonsNumbersAndSymbols[2], fourth: buttonsNumbersAndSymbols[3])
+        let secondCell = createStackNumbers(first: buttonsNumbersAndSymbols[4], second: buttonsNumbersAndSymbols[5], third: buttonsNumbersAndSymbols[6], fourth: buttonsNumbersAndSymbols[7])
+        let thirdCell = createStackNumbers(first: buttonsNumbersAndSymbols[8], second: buttonsNumbersAndSymbols[9], third: buttonsNumbersAndSymbols[10], fourth: buttonsNumbersAndSymbols[11])
+        let fourthCell = createStackNumbers(first: buttonsNumbersAndSymbols[12], second: buttonsNumbersAndSymbols[13], third: buttonsNumbersAndSymbols[14], fourth: buttonsNumbersAndSymbols[15])
+        
+        mainStackView.addArrangedSubview(firstCell)
+        mainStackView.addArrangedSubview(secondCell)
+        mainStackView.addArrangedSubview(thirdCell)
+        mainStackView.addArrangedSubview(fourthCell)
+        mainStackView.addArrangedSubview(createLastStack())
+        
+    }
+    
+    private func createLastStack() -> UIView{
+        let lastStack = UIStackView()
+        lastStack.axis = .horizontal
+        lastStack.spacing = 8
+        lastStack.addArrangedSubview(buttonsNumbersAndSymbols[16])
+        lastStack.addArrangedSubview(buttonsNumbersAndSymbols[17])
+        lastStack.addArrangedSubview(buttonsNumbersAndSymbols[18])
+        NSLayoutConstraint.activate([
+            buttonsNumbersAndSymbols[16].widthAnchor.constraint(equalTo: lastStack.widthAnchor, multiplier: 0.5),
+            buttonsNumbersAndSymbols[17].widthAnchor.constraint(equalTo: lastStack.widthAnchor, multiplier: 0.23),
+            buttonsNumbersAndSymbols[18].widthAnchor.constraint(equalTo: lastStack.widthAnchor, multiplier: 0.23)
+        ])
+        return lastStack
+    }
+    
+    private func createStackNumbers(first: UIButton, second: UIButton, third : UIButton, fourth: UIButton) -> UIView{
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.addArrangedSubview(first)
+        stackView.addArrangedSubview(second)
+        stackView.addArrangedSubview(third)
+        stackView.addArrangedSubview(fourth)
+        return stackView
+    }
+    
+    private func createBasicButton(for number: String, with color: UIColor, tamic: Bool = true) -> UIButton{
+        let button = UIButton()
+        button.setTitle(number, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = tamic
+        var configuration = UIButton.Configuration.plain()
+        configuration.background.backgroundColor = color
+        configuration.baseForegroundColor = .systemGray6
+        configuration.cornerStyle = .capsule
+        button.configuration = configuration
+        return button
     }
 }
--- BasiCalculator/CalculatorViewController.swift
+++ BasiCalculator/CalculatorViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     private var customView: CalculatorView {
         return view as! CalculatorView
     }
@@ -17,11 +17,101 @@ class CalculatorViewController: UIViewController {
         view = CalculatorView()
     }
     
+    var firstNumber: Float = 0
+    var secondNumber: Float = 0
+    var currentOperation: Operations = .none
+    
+    enum Operations {
+        case add, subtract, multiply, divide, none
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         customView.backgroundColor = .systemBackground
+        customView.buttonsNumbersAndSymbols[0].addAction(UIAction(handler: resetDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[1].addTarget(self, action: #selector(doOperation(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[2].addTarget(self, action: #selector(doOperation(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[3].addTarget(self, action: #selector(doOperation(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[4].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[5].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[6].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[7].addTarget(self, action: #selector(doOperation(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[8].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[9].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[10].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[11].addTarget(self, action: #selector(doOperation(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[12].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[13].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[14].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[15].addTarget(self, action: #selector(doOperation(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[16].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[17].addTarget(self, action: #selector(updateDisplay(_:)), for: .touchUpInside)
+        customView.buttonsNumbersAndSymbols[18].addTarget(self, action: #selector(doOperation(_:)), for: .touchUpInside)
     }
-
-
+    
+    @objc func updateDisplay(_ sender: UIButton){
+        if customView.labelResult.text! == "0"{
+            customView.labelResult.text! = sender.titleLabel!.text!
+        }else{
+            customView.labelResult.text! += sender.titleLabel!.text!
+        }
+    }
+    
+    private func resetDisplay(_ sender: UIAction){
+        customView.labelResult.text = "0"
+        firstNumber = 0
+        secondNumber = 0
+        currentOperation = .none
+    }
+    
+    @objc func doOperation(_ sender: UIButton){
+        switch sender.titleLabel!.text!{
+        case "/":
+            firstNumber = Float(customView.labelResult.text!) ?? 0
+            currentOperation = .divide
+        case "%":
+            break
+        case "x":
+            firstNumber = Float(customView.labelResult.text!) ?? 0
+            currentOperation = .multiply
+        case "-":
+            firstNumber = Float(customView.labelResult.text!) ?? 0
+            currentOperation = .subtract
+        case "+":
+            firstNumber = Float(customView.labelResult.text!)!
+            currentOperation = .add
+        case "+/-":
+            break
+        default:
+            secondNumber = Float(customView.labelResult.text!) ?? 0
+            switch currentOperation {
+            case .add:
+                customView.labelResult.text = isDecimalOrInt(this: (firstNumber + secondNumber))
+            case .subtract:
+                customView.labelResult.text = isDecimalOrInt(this: (firstNumber - secondNumber))
+            case .multiply:
+                customView.labelResult.text = isDecimalOrInt(this: (firstNumber * secondNumber))
+            case .divide:
+                customView.labelResult.text = isDecimalOrInt(this: (firstNumber / secondNumber))
+            case .none:
+                break
+            }
+        }
+        
+        if sender.titleLabel!.text! != "="{
+            customView.labelResult.text = ""
+        }
+        
+    }
+    
+    private func isDecimalOrInt(this number: Float) -> String{
+        if number.truncatingRemainder(dividingBy: 1) == 0 {
+            return "\(Int(number))"
+        } else {
+            return "\(number)"
+        }
+    }
 }
+
+
 
